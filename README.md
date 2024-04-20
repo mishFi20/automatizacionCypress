ddd4eec3b59841699a87dbb071d4dd7fb8e37681
mishFi20 <dsljdlls>
2024-04-19 21:04:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# AUTOMATIZACION CYPRESS
+## REQUERIMIENTOS
+- cypress version 13
+- node version 18 
+- visual studio Code 
+
+## pasos para correr el proyecto
+-Descargue el proyecto `git remote add origin https://github.com/mishFi20/automatizacionCypress.git`
+-Abrir la solucion con Visual Studio Code 
+-Ejecutar la solucion
+
+
+## Contribucion
+- Crear una nueva rama debidamente identificada: featue, fix, doc
+- Realizar los cambios necesarios
+- Subir la rama al repositorio
+- Abrir un PR a la rama main
---
+++ cypress/e2e/youtube.cy.js
@@ -0,0 +1,19 @@
+describe('Buscando un video en Youtube', function() {
+    it.skip('Buscar el video de cypress', function() {
+        cy.visit("https://youtube.com");
+        cy.get("input#search").click();
+        cy.get("input#search").type("cypress js en español {enter}");
+        cy.get("#video-title").contains("🤖 Tutorial de Cypress | Cypress Testing");
+        cy.get("#video-title").contains("🤖 Tutorial de Cypress | Cypress Testing").click();
+    });
+    // Borrar historial de reproducciones
+    describe('Eliminar historial de reproducciones', function(){
+        it('Borrar todo el historial de reproducciones en YouTube', function(){
+            cy.visit("https://youtube.com");
+            cy.get('a#endpoint[href="/feed/history"]').click();
+            cy.get('button[aria-label="Borrar todo el historial de reproducciones"]').click();
+            cy.get('button[aria-label="Borrar historial de reproducciones"]').click();
+            cy.get("#message").contains("Esta lista no tiene videos.").should('be.visible');
+        })
+    })
+});
